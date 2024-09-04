80beba9151068866897a65aa83c326e2b251f8b6
brunamiyagi <bmmacedo9@gmail.com>
2024-09-03 21:50:16 -0300

    Inicio de arquivo js

---
+++ Projeto/script/index.js
@@ -0,0 +1,8 @@
+console.log('teste');
+
+const produtosCarrinho = document.querySelector('.meu-carrinho-produtos')
+const btnAdiconarCarrinho = document.querySelector('#btn-adicionar-carrinho')
+
+btnAdiconarCarrinho.addEventListener('click', ()=>{
+    let produtoAdicionado = document.createElement('')
+})
